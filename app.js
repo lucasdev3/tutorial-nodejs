ad353a97ec982b98af24817f3fcb5fe8ac1f4d0a
Lucas Souza <79222458+lucasdev3@users.noreply.github.com>
2022-01-22 09:22:25 -0300

    update v1.0
    implementação em nodejs

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;
+app.use(express.static(__dirname + "/public/css/"));
+app.use(express.static(__dirname + "/public/icons/"));
+app.use(express.static(__dirname + "/public/todolist/"));
+app.use(express.static(__dirname + "/public/jogodanave/"));
+
+const port = process.env.PORT || 8080;
+
+
+
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/index.html");
-})
+});
 
-app.get('/contato', function (req, res) {
-    res.send('Página de contato');
-  })
+app.get('/todolist', function (req, res) {
+    res.sendFile(__dirname + "/public/templates/todolist.html");
+  });
+
+app.get('/jogodanave', function (req, res) {
+  res.sendFile(__dirname + "/public/templates/jogodanave.html");
+});
+
 
 app.listen(port, () => {
-    console.log("Servidor iniciado na porta 8080: https://localhost:8080");
+    console.log(`Servidor iniciado na porta ${port}: https://localhost:8080`);
 });
+
+
